785688d3ce722769551e2f8ca444e7dac91148f9
sevenprog <fuadimukhlik.ac.id@students.amikom.ac.id>
2021-12-08 20:41:38 +0700

    first commit

---
+++ PraktikumGit-master/PraktikumGit/PraktikumGit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PraktikumGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pemrograman Lanjut S1 Informatika");
+            Console.WriteLine("Materi Praktikum Git");
+        }
+    }
+}
